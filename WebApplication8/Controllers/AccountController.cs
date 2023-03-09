999d07ae21d48cde215c73060703cb75098b4765
reza-hasan <ahrea47@gmail.com>
2023-03-09 20:36:27 +0600

    first

---
+++ WebApplication8/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login( Models.Membership model)
+        {
+            using (var context = new OfficeEntities())
+            {
+
+                bool isValid = context.User.Any(x=>x.UserName == model.UserName && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+
+                ModelState.AddModelError("", "Invalid Username or password");
+                return View();
+
+            }
+            
+        }
+
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+
+            using (var context = new OfficeEntities())
+            {
+
+                context.User.Add(model);
+                context.SaveChanges();
+
+
+
+            }
+
+            return RedirectToAction("login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+        [HttpPost,Authorize]
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication8/Models/UserProfileModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8.Models
+{
+    public class UserProfileModel
+    {
+
+        [Display(Name = "Full Name:")]
+        [Required(ErrorMessage ="Full Name is required.")]
+        public string FullName { get; set; }
+
+        
+
+        [Display(Name = "Pass:")]
+        [Required(ErrorMessage = "pass is required.")]
+        public string Address { get; set; }
+
+    }
+}
